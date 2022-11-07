8ce630ade96a1db8e26276d347b0a421a797688b
Martin Morgan <Martin.Morgan@RoswellPark.org>
2022-11-07 09:14:51 -0500

    use IS_BIOC_BUILD_MACHINE instead of BBS_HOME

--- R/utilities.R
+++ R/utilities.R
@@ -26,17 +26,14 @@
     function(libname, pkgname)
 {
     ## reset the cache once a week on the builders
-    ##
-    ## this test is from testthat::skip_on_bioc
-    if (identical(Sys.getenv("BBS_HOME"), "")) {
-        return()
-    }
-    cache_path <- .cellxgene_cache_path()
-    if (dir.exists(cache_path)) {
-        creation_time <-  file.info(cache_path, extra_cols = FALSE)$ctime
-        age <- difftime(Sys.Date(), creation_time, units = "days")
-        if (age > 7) {
-            unlink(cache_path, recursive = TRUE, force =  TRUE)
+    if (identical(Sys.getenv("IS_BIOC_BUILD_MACHINE"), "true")) {
+        cache_path <- .cellxgene_cache_path()
+        if (dir.exists(cache_path)) {
+            creation_time <-  file.info(cache_path, extra_cols = FALSE)$ctime
+            age <- difftime(Sys.Date(), creation_time, units = "days")
+            if (age > 7) {
+                unlink(cache_path, recursive = TRUE, force =  TRUE)
+            }
         }
     }
 }
